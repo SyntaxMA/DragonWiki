d40a20fd421f70bca3bfdb0f82243bdc6b0f21a2
juego <maciasalvarezruben@elpuig.xeill.net>
2022-02-15 19:10:28 +0100

    Version 0.6.1

---
+++ app/src/main/java/com/example/dragonwiki/MenuInferior_fragment.java
@@ -0,0 +1,58 @@
+package com.example.dragonwiki;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+public class MenuInferior_fragment extends Fragment {
+
+    NavController navController;
+
+    ImageView inicio;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_menu_inferior, container, false);
+    }
+
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        navController = Navigation.findNavController(view);
+
+        inicio = view.findViewById(R.id.radar3);
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_menuInferior_fragment_to_menu_fragment);
+            }
+        });
+        inicio = view.findViewById(R.id.iniciotext);
+        inicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                navController.navigate(R.id.action_menuInferior_fragment_to_menu_fragment);
+            }
+        });
+    }
+}
